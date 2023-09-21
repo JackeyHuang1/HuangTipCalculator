2f7b92b2deeff90f9a073423af74da3059d9a20d
BT_1N3_20 <jackeyh2@nycstudents.net>
2023-09-21 08:36:42 -0400

    initial commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,21 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
+public class TipCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Welcome to the tip calculator!");
+        System.out.print("How many people are in your group: ");
+        int groupNum = scan.nextInt();
+        System.out.print("What's the tip percentage? (0-100): ");
+        int tipNum = scan.nextInt();
+
+        while (number % 2 != 0) {
+            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end);
+            number = scan.nextInt();
+        }
+        System.out.print("Done!");
+
+    }
+}
